a7eae7c6fa3e622e8bc801b46450571b23151a9b
GabrielBza <Gabrielbzandrade@gmail.com>
2025-11-12 09:31:24 -0300

    Reajuste GridCards/PluggyButton

--- components/home/GridCards.tsx
+++ components/home/GridCards.tsx
@@ -1,3 +1,5 @@
+import PluggyButton from "@/components/home/PluggyButton";
+
 export const CardsGrid = () => {
   return (
     <>
@@ -30,6 +32,7 @@ export const CardsGrid = () => {
           <div className='lg:col-span-5'>
             <div className='h-[280px] rounded-lg border bg-white p-6 shadow-sm'>
               <h2 className='text-lg font-semibold'>Minhas contas</h2>
+              <PluggyButton />
               {/* TODO: Adicionar conteúdo */}
             </div>
           </div>
